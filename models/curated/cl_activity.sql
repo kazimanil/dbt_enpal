7e958f4bda45eaa5fd7e4a20df70378acfb9fe3e
kazimanil <kazimanileren@gmail.com>
2025-12-10 16:49:46 +0100

    add unique id to activities

--- models/curated/cl_activity.sql
+++ models/curated/cl_activity.sql
@@ -1,2 +1,9 @@
-SELECT DISTINCT *
-FROM {{ source('enpal', 'activity') }}
+WITH deduplicated AS (
+    SELECT DISTINCT *
+    FROM {{ source('enpal', 'activity') }}
+)
+
+SELECT
+    CONCAT(activity_id, '_', due_to) AS unique_activity_id,
+    *
+FROM deduplicated
