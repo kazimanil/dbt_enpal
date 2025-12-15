748f0fb0fc450e3354addd2dfab32f52da13bb4c
kazimanil <kazimanileren@gmail.com>
2025-12-15 16:49:21 +0100

    add rn for repeating steages

--- models/curated/cl_deals.sql
+++ models/curated/cl_deals.sql
@@ -1,9 +1,19 @@
 WITH deduplicated AS (
     SELECT DISTINCT *
     FROM {{ source('enpal', 'deal_changes') }}
+),
+
+row_numbers AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (
+            PARTITION BY deal_id, changed_field_key, new_value
+            ORDER BY change_time DESC
+        ) AS rn
+    FROM deduplicated
 )
 
 SELECT
-    CONCAT(deal_id, '_', changed_field_key, '_', new_value) AS unique_activity_id,
+    CONCAT(deal_id, '_', changed_field_key, '_', new_value, '_', rn) AS unique_activity_id,
     *
-FROM deduplicated
+FROM row_numbers
